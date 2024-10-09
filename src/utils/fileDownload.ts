4921913076448d57f5f62815ca5b33ce0c1b2172
dhkim <ddhkim11@gmail.com>
2024-10-09 14:15:22 +0900

    파일 다운로드 함수 수정

--- src/utils/fileDownload.ts
+++ src/utils/fileDownload.ts
@@ -1,4 +1,12 @@
-export const handleDownload = (fileName: string) => {
+import { RESOURCES } from "../constants/resources";
+
+type Resources = {
+  DOCS: { [key: string]: string };
+};
+
+export const handleDownload = (resourceKey: string) => {
+  const fileName = (RESOURCES as Resources)['DOCS'][resourceKey];
+
   const link = document.createElement("a");
   link.href = `/documents/${fileName}`;
   link.download = fileName;
