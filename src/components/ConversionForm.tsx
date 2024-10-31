4075fa897a3597e9a0c4eec60605fbf8071f5f91
dhkim <ddhkim11@gmail.com>
2024-10-31 13:53:32 +0900

    ..

--- src/components/ConversionForm.tsx
+++ src/components/ConversionForm.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { Form, Select, Input, DatePicker, Button, FormInstance } from "antd";
 import {
-  FileTextOutlined,
-  CalendarOutlined,
-  ClockCircleOutlined,
   UserOutlined,
   CheckSquareOutlined,
+  CalendarOutlined,
+  ClockCircleOutlined,
+  FileTextOutlined,
+  FileDoneOutlined,
 } from "@ant-design/icons";
 import {
   FormValues,
@@ -43,8 +44,120 @@ const ConversionForm: React.FC<ConversionFormProps> = ({
   isLoading,
 }) => {
   const conversionType = Form.useWatch("conversionType", form);
-  const isVacationType = conversionType === "vacation";
-  const isOfficialLeaveType = conversionType === "officialLeave";
+
+  const renderAttendanceFields = () => (
+    <>
+      <div css={flexLayout.container}>
+        <Form.Item
+          name="checkInTime"
+          label={
+            <FormLabel icon={<ClockCircleOutlined />}>입실 시간</FormLabel>
+          }
+          rules={[
+            {
+              required: true,
+              message: FEEDBACK_MESSAGES.FORM_VALIDATION.CHECKIN_TIME_REQUIRED,
+            },
+          ]}
+          css={halfWidth}
+        >
+          <Input placeholder="ex) 10:00" />
+        </Form.Item>
+
+        <Form.Item
+          name="checkOutTime"
+          label={
+            <FormLabel icon={<ClockCircleOutlined />}>퇴실 시간</FormLabel>
+          }
+          rules={[
+            {
+              required: true,
+              message: FEEDBACK_MESSAGES.FORM_VALIDATION.CHECKOUT_TIME_REQUIRED,
+            },
+          ]}
+          css={halfWidth}
+        >
+          <Input placeholder="ex) 19:00" />
+        </Form.Item>
+      </div>
+
+      <Form.Item
+        name="reason"
+        label={<FormLabel icon={<FileTextOutlined />}>사유</FormLabel>}
+        rules={[
+          {
+            required: true,
+            message: FEEDBACK_MESSAGES.FORM_VALIDATION.REASON_REQUIRED,
+          },
+        ]}
+      >
+        <TextArea
+          rows={1}
+          placeholder="ex) HRD 오류, 면접, 시험, 질병, 예비군 ..."
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="proofDocumentName"
+        label={<FormLabel icon={<FileTextOutlined />}>증빙서류명</FormLabel>}
+      >
+        <Input placeholder="ex) 진료확인서, 면접확인서, 예비군 필증..." />
+      </Form.Item>
+    </>
+  );
+
+  const renderVacationFields = () => (
+    <>
+      <Form.Item
+        name="courseContent"
+        label={
+          <FormLabel icon={<FileTextOutlined />}>
+            불참하는 과정 교육내용
+          </FormLabel>
+        }
+        rules={[
+          {
+            required: true,
+            message: FEEDBACK_MESSAGES.FORM_VALIDATION.FIELD_REQUIRED,
+          },
+        ]}
+      >
+        <TextArea rows={1} placeholder="내용 작성" />
+      </Form.Item>
+
+      <Form.Item
+        name="studyPlan"
+        label={
+          <FormLabel icon={<FileTextOutlined />}>학습 진행 계획</FormLabel>
+        }
+        rules={[
+          {
+            required: true,
+            message: FEEDBACK_MESSAGES.FORM_VALIDATION.FIELD_REQUIRED,
+          },
+        ]}
+      >
+        <TextArea rows={1} placeholder="자세히 작성" />
+      </Form.Item>
+
+      <Form.Item
+        name="significant"
+        label={<FormLabel icon={<FileTextOutlined />}>특이사항</FormLabel>}
+      >
+        <TextArea rows={1} placeholder="없을 시 생략 가능" />
+      </Form.Item>
+    </>
+  );
+
+  const renderOfficialLeaveFields = () => (
+    <Form.Item
+      name="proofDocumentName"
+      label={<FormLabel icon={<FileTextOutlined />}>증빙서류명</FormLabel>}
+      style={{ marginTop: "60px", marginBottom: "74px" }}
+    >
+      <Input placeholder="ex) 진료확인서, 면접확인서, 예비군 필증..." />
+    </Form.Item>
+  );
 
   return (
     <Form
@@ -82,7 +195,9 @@ const ConversionForm: React.FC<ConversionFormProps> = ({
           },
         ]}
         style={
-          isOfficialLeaveType ? { marginTop: "60px", marginBottom: "74px" } : {}
+          conversionType === "officialLeave"
+            ? { marginTop: "60px", marginBottom: "74px" }
+            : {}
         }
       >
         <Input placeholder="이름 입력" />
@@ -101,128 +216,16 @@ const ConversionForm: React.FC<ConversionFormProps> = ({
             message: FEEDBACK_MESSAGES.FORM_VALIDATION.DATE_REQUIRED,
           },
         ]}
-        style={isOfficialLeaveType ? { marginBottom: "74px" } : {}}
+        style={
+          conversionType === "officialLeave" ? { marginBottom: "74px" } : {}
+        }
       >
         <DatePicker css={datePicker} />
       </Form.Item>
 
-      {!isVacationType && !isOfficialLeaveType && (
-        <>
-          <div css={flexLayout.container}>
-            <Form.Item
-              name="checkInTime"
-              label={
-                <FormLabel icon={<ClockCircleOutlined />}>입실 시간</FormLabel>
-              }
-              rules={[
-                {
-                  required: true,
-                  message:
-                    FEEDBACK_MESSAGES.FORM_VALIDATION.CHECKIN_TIME_REQUIRED,
-                },
-              ]}
-              css={halfWidth}
-            >
-              <Input placeholder="ex) 10:00" />
-            </Form.Item>
-
-            <Form.Item
-              name="checkOutTime"
-              label={
-                <FormLabel icon={<ClockCircleOutlined />}>퇴실 시간</FormLabel>
-              }
-              rules={[
-                {
-                  required: true,
-                  message:
-                    FEEDBACK_MESSAGES.FORM_VALIDATION.CHECKOUT_TIME_REQUIRED,
-                },
-              ]}
-              css={halfWidth}
-            >
-              <Input placeholder="ex) 19:00" />
-            </Form.Item>
-          </div>
-
-          <Form.Item
-            name="reason"
-            label={<FormLabel icon={<FileTextOutlined />}>사유</FormLabel>}
-            rules={[
-              {
-                required: true,
-                message: FEEDBACK_MESSAGES.FORM_VALIDATION.REASON_REQUIRED,
-              },
-            ]}
-          >
-            <TextArea
-              rows={1}
-              placeholder="ex) HRD 오류, 면접, 시험, 질병, 예비군 ..."
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="proofDocumentName"
-            label={
-              <FormLabel icon={<FileTextOutlined />}>증빙서류명</FormLabel>
-            }
-          >
-            <Input placeholder="ex) 진료확인서, 면접확인서, 예비군 필증..." />
-          </Form.Item>
-        </>
-      )}
-
-      {isVacationType && (
-        <>
-          <Form.Item
-            name="courseContent"
-            label={
-              <FormLabel icon={<FileTextOutlined />}>
-                불참하는 과정 교육내용
-              </FormLabel>
-            }
-            rules={[
-              {
-                required: true,
-                message: FEEDBACK_MESSAGES.FORM_VALIDATION.FIELD_REQUIRED,
-              },
-            ]}
-          >
-            <TextArea rows={1} placeholder="내용 작성" />
-          </Form.Item>
-
-          <Form.Item
-            name="studyPlan"
-            label={
-              <FormLabel icon={<FileTextOutlined />}>학습 진행 계획</FormLabel>
-            }
-            rules={[
-              {
-                required: true,
-                message: FEEDBACK_MESSAGES.FORM_VALIDATION.FIELD_REQUIRED,
-              },
-            ]}
-          >
-            <TextArea rows={1} placeholder="자세히 작성" />
-          </Form.Item>
-
-          <Form.Item
-            name="significant"
-            label={<FormLabel icon={<FileTextOutlined />}>특이사항</FormLabel>}
-          >
-            <TextArea rows={1} placeholder="없을 시 생략 가능" />
-          </Form.Item>
-        </>
-      )}
-
-      {isOfficialLeaveType && (
-        <Form.Item
-          name="proofDocumentName"
-          label={<FormLabel icon={<FileTextOutlined />}>증빙서류명</FormLabel>}
-          style={{ marginBottom: "74px" }}
-        >
-          <Input placeholder="ex) 진료확인서, 면접확인서, 예비군 필증..." />
-        </Form.Item>
-      )}
+      {conversionType === "attendance" && renderAttendanceFields()}
+      {conversionType === "vacation" && renderVacationFields()}
+      {conversionType === "officialLeave" && renderOfficialLeaveFields()}
 
       <FileUpload
         fileList={fileList}
@@ -236,6 +239,7 @@ const ConversionForm: React.FC<ConversionFormProps> = ({
           htmlType="submit"
           css={submitButton}
           disabled={isLoading}
+          icon={<FileDoneOutlined />}
         >
           실행
         </Button>
--- src/components/GuideForm.tsx
+++ src/components/GuideForm.tsx
@@ -32,112 +32,113 @@ const GuideForm: React.FC = () => {
   const [form] = Form.useForm();
   const signatureRef = useRef<SignatureCanvas>(null);
 
+  const renderDownloadButtons = () => (
+    <Form.Item
+      label={
+        <FormLabel icon={<CloudDownloadOutlined />}>문서 다운로드</FormLabel>
+      }
+    >
+      <div css={buttonGroup}>
+        {DOCS.map((doc, index) => (
+          <Button
+            key={index}
+            icon={<DownloadOutlined />}
+            onClick={() => handleDownload(doc)}
+            css={downloadButton}
+          >
+            {index === 0 ? "출석대장 다운로드" : "휴가계획서 다운로드"}
+          </Button>
+        ))}
+      </div>
+    </Form.Item>
+  );
+
+  const renderSignatureField = () => (
+    <Form.Item label={<FormLabel icon={<EditOutlined />}>서명</FormLabel>}>
+      <Space direction="vertical" size="middle" css={fullWidth}>
+        <div css={flexLayout.center}>
+          <SignatureCanvas
+            ref={signatureRef}
+            canvasProps={{
+              width: 540,
+              height: 200,
+              className: "signature-canvas",
+              style: signatureCanvas,
+            }}
+          />
+        </div>
+        <div css={[flexLayout.end, fullWidth, spacing.margin.bottom.sm]}>
+          <Space wrap>
+            <Button
+              icon={<DownloadOutlined />}
+              onClick={() => handleSignatureDownload(signatureRef)}
+            >
+              서명 다운로드
+            </Button>
+            <Button
+              icon={<RedoOutlined />}
+              onClick={() => handleSignatureClear(signatureRef)}
+            >
+              초기화
+            </Button>
+          </Space>
+        </div>
+      </Space>
+    </Form.Item>
+  );
+
+  const renderGuidelinesAlert = () => (
+    <Alert
+      description={
+        <>
+          {[
+            "HRD 관련 오류는 증빙서류명 작성 생략",
+            "증빙서류는 이미지 파일 형식으로 첨부",
+            "서명 이미지는 'sign.png'로 첨부",
+          ].map((text, index) => (
+            <Typography.Text key={index} css={typographyTextStyle}>
+              {`${index + 1}. ${text}`}
+              <br />
+            </Typography.Text>
+          ))}
+        </>
+      }
+      type="warning"
+      showIcon
+      css={spacing.margin.bottom.lg}
+    />
+  );
+
+  const renderLinks = () => (
+    <Form.Item label={<FormLabel icon={<LinkOutlined />}>링크</FormLabel>}>
+      <div css={buttonGroup}>
+        <Button
+          href={NOTICELINK}
+          target="_blank"
+          css={downloadButton}
+          icon={<NotificationOutlined />}
+        >
+          공지사항
+        </Button>
+        <Button
+          href={FORMLINK}
+          target="_blank"
+          css={downloadButton}
+          icon={<SendOutlined />}
+        >
+          제출하기
+        </Button>
+      </div>
+    </Form.Item>
+  );
+
   return (
     <Form form={form} layout="vertical" css={flexLayout.column}>
       <div css={flexLayout.flex00Auto}>
-        <Form.Item
-          label={
-            <FormLabel icon={<CloudDownloadOutlined />}>
-              문서 다운로드
-            </FormLabel>
-          }
-        >
-          <div css={buttonGroup}>
-            <Button
-              icon={<DownloadOutlined />}
-              onClick={() => handleDownload(DOCS[0])}
-              css={downloadButton}
-            >
-              출석대장 다운로드
-            </Button>
-            <Button
-              icon={<DownloadOutlined />}
-              onClick={() => handleDownload(DOCS[1])}
-              css={downloadButton}
-            >
-              휴가계획서 다운로드
-            </Button>
-          </div>
-        </Form.Item>
-
-        <Form.Item label={<FormLabel icon={<EditOutlined />}>서명</FormLabel>}>
-          <Space direction="vertical" size="middle" css={fullWidth}>
-            <div css={flexLayout.center}>
-              <SignatureCanvas
-                ref={signatureRef}
-                canvasProps={{
-                  width: 540,
-                  height: 200,
-                  className: "signature-canvas",
-                  style: signatureCanvas,
-                }}
-              />
-            </div>
-            <div css={[flexLayout.end, fullWidth, spacing.margin.bottom.sm]}>
-              <Space wrap>
-                <Button
-                  icon={<DownloadOutlined />}
-                  onClick={() => handleSignatureDownload(signatureRef)}
-                >
-                  서명 다운로드
-                </Button>
-                <Button
-                  icon={<RedoOutlined />}
-                  onClick={() => handleSignatureClear(signatureRef)}
-                >
-                  초기화
-                </Button>
-              </Space>
-            </div>
-          </Space>
-        </Form.Item>
-
-        <Alert
-          description={
-            <>
-              <Typography.Text css={typographyTextStyle}>
-                1. HRD 관련 오류는 증빙서류명 작성 생략
-              </Typography.Text>
-              <br />
-              <Typography.Text css={typographyTextStyle}>
-                2. 증빙서류는 이미지 파일 형식으로 첨부
-              </Typography.Text>
-              <br />
-              <Typography.Text css={typographyTextStyle}>
-                3. 서명 이미지는 'sign.png'로 첨부
-              </Typography.Text>
-            </>
-          }
-          type="warning"
-          showIcon
-          css={spacing.margin.bottom.lg}
-        />
-
-        <div css={flexLayout.flex00Auto}>
-          <Form.Item
-            label={<FormLabel icon={<LinkOutlined />}>링크</FormLabel>}
-          >
-            <div css={buttonGroup}>
-              <Button
-                href={NOTICELINK}
-                target="_blank"
-                css={downloadButton}
-                icon={<NotificationOutlined />}
-              >
-                공지사항
-              </Button>
-
-              <Button
-                href={FORMLINK}
-                target="_blank"
-                css={downloadButton}
-                icon={<SendOutlined />}
-              >
-                제출하기
-              </Button>
-            </div>
-          </Form.Item>
-        </div>
+        {renderDownloadButtons()}
+        {renderSignatureField()}
+        {renderGuidelinesAlert()}
+        {renderLinks()}
       </div>
     </Form>
   );
